b7dab079f9e694376dad180ec4858b43c9ebb449
yerxichen <879908188@qq.com>
2020-05-18 10:15:30 +0800

    整合uview

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,46 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view>
+            <text class="title">{{title}}</text>
+        </view>
+        <u-button>默认按钮</u-button>
+        <u-button type="primary">主要按钮</u-button>
+        <u-button type="success">成功按钮</u-button>
+        <u-button type="info">信息按钮</u-button>
+        <u-button type="warning">警告按钮</u-button>
+        <u-button type="error">危险按钮</u-button>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+    export default {
+        data() {
+            return {
+                title: 'Hello'
+            }
+        },
+        onLoad() {
 
-		},
-		methods: {
-
-		}
-	}
+        },
+        methods: {}
+    }
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+    .content {
+        text-align: center;
+        height: 400 upx;
+    }
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
+    .logo {
+        height: 200 upx;
+        width: 200 upx;
+        margin-top: 200 upx;
+    }
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+    .title {
+        font-size: 36 upx;
+        color: #8f8f94;
+    }
 </style>
